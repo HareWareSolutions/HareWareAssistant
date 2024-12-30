6a9b9a89df1b3a4bcfe3212d73cb70ddce93aa2b
giordanoenzo <enzogiordanoofficial@gmail.com>
2024-12-29 23:43:51 -0300

    relatorio alteracao

--- app/main.py
+++ app/main.py
@@ -573,12 +573,7 @@ async def relatorio_agendamento(empresa: str, nome_empresa: str, data: str):
                 "Content-Disposition": f"attachment; filename={nome_arquivo}"
             })
 
-            @response.call_on_close
-            def remove_file():
-                try:
-                    os.remove(caminho_pdf)
-                except Exception as e:
-                    print(f"Erro ao excluir o arquivo {caminho_pdf}: {e}")
+
 
             return response
         else:
