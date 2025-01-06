6b685a0e67afa5d7be7d1771ebf89ed1c8fb2264
giordanoenzo <enzogiordanoofficial@gmail.com>
2025-01-06 20:39:43 -0300

    seguranca zapi

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://api.z-api.io"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
