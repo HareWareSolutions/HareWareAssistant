fff3d783d8119c790b4d4b369f29fa625864dd4a
giordanoenzo <enzogiordanoofficial@gmail.com>
2025-01-23 22:08:32 -0300

    teste versao antiga

--- app_v1/main.py
+++ app_v1/main.py
@@ -73,16 +73,20 @@ async def receive_message(request: Request):
     data = await request.json()
 
     # Printando os dados recebidos na requisição
-    print("Dados recebidos:", data)
 
-    """api_url = "https://host13.serverapi.dev"  # Substitua com o seu host real
+    numero = data.get("remoteJid")
+
+    numero = numero[0:11]
+
+
+    api_url = "https://host13.serverapi.dev"  # Substitua com o seu host real
     connection_key = "w-api_N3IE7GZOFN"  # Substitua com a chave de conexão real
-    phone_number = "5519997581672"  # Substitua com o número de telefone real
+    phone_number = numero  # Substitua com o número de telefone real
     message = "oi, teste"  # A mensagem que você deseja enviar
     delay_message = 1000  # Atraso de 1000 milissegundos
     auth_token = "xT3AcKpnGLC5VPk49fhTlCLwk1VkuU9Up"  # Substitua com seu token de autenticação
 
     # Chamando a função para enviar a mensagem
-    resultado = await enviar_mensagem(api_url, connection_key, phone_number, message, delay_message, auth_token)"""
+    resultado = await enviar_mensagem(api_url, connection_key, phone_number, message, delay_message, auth_token)
 
     return {"status": "success", "message": "Mensagem recebida com sucesso! Este é um teste."}
