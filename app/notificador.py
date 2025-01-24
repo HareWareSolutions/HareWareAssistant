3000de1fcef7db33d2c0bf92a6e26ae83179b27f
giordanoenzo <enzogiordanoofficial@gmail.com>
2025-01-24 15:44:35 -0300

    notificacoes ajuste

--- app/notificador.py
+++ app/notificador.py
@@ -2,6 +2,7 @@ import sys
 import os
 import pytz
 from datetime import datetime, timedelta
+import time
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
@@ -10,11 +11,11 @@ from models.agendamento import buscar_agendamentos_por_data_ntf
 from models.contato import buscar_contato_id
 from utils.zapi import send_message_zapi
 
+notificacoes_enviadas = {}
 
 def get_hora_brasil():
     brasil_tz = pytz.timezone('America/Sao_Paulo')
     return datetime.now(brasil_tz)
-
 
 def mensagem_env(env, nome, hora):
     match env:
@@ -31,26 +32,32 @@ def mensagem_env(env, nome, hora):
                     f'Hoje você tem um compromisso marcado na barbearia do Malaman às {hora}.\n\n'
                     f'Contamos com a sua presença! até breve!')
 
-
 def is_round_hour():
     current_time = get_hora_brasil()
     return current_time.minute == 0 and current_time.second == 0
-
 
 def notificar():
     envs = ['hareware']
 
     while True:
         if is_round_hour():
+            agora = get_hora_brasil()
+            for agendamento_id, hora_notificada in list(notificacoes_enviadas.items()):
+                if agora - hora_notificada > timedelta(hours=1):
+                    notificacoes_enviadas.pop(agendamento_id)
+
             for env in envs:
                 db = next(get_db(env))
                 try:
-                    hoje = get_hora_brasil().date()
+                    hoje = agora.date()
                     agendamentos = buscar_agendamentos_por_data_ntf(db, hoje)
-                    hora_atual = get_hora_brasil()
+                    hora_atual = agora
 
                     for agendamento in agendamentos:
-                        hora_agendada = datetime.combine(hoje, agendamento.hora)  # Combina data e hora
+                        if agendamento.id in notificacoes_enviadas:
+                            continue
+
+                        hora_agendada = datetime.combine(hoje, agendamento.hora)
                         hora_agendada = hora_agendada.replace(
                             tzinfo=pytz.timezone('America/Sao_Paulo').localize(datetime.now()).tzinfo
                         )
@@ -73,8 +80,9 @@ def notificar():
                                 delay_typing=2
                             )
 
+                            notificacoes_enviadas[agendamento.id] = agora
+
                 finally:
                     db.close()
-
-
-notificar()
+        else:
+            time.sleep(30)
