67c9b15ddf946a95862ad6ef96ac06e5a2832fa4
giordanoenzo <enzogiordanoofficial@gmail.com>
2025-01-28 16:29:00 -0300

    options

--- app/main.py
+++ app/main.py
@@ -28,7 +28,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://api.z-api.io", "http://hareinteract.com.br", "https://hareinteract.com.br", "https://hareware.com.br"],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
 )
 
