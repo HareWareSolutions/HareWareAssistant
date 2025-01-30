b6f7246eba998f84824f5ba3071f17e5b49138d7
giordanoenzo <enzogiordanoofficial@gmail.com>
2025-01-30 16:55:41 -0300

    correcao horas

--- app/utils/rotinasHoras.py
+++ app/utils/rotinasHoras.py
@@ -1,14 +1,12 @@
 from datetime import datetime
 import pytz
 
-
 def verificar_horarios(env, agendamentos, data_agendamento):
-    print(agendamentos)
     horarios_disponiveis = {
-        'hareware': [{"08:00": 1}, {"09:00": 1}, {"10:00": 1}, {"11:00": 1}, {"13:00": 1}, {"14:00": 1}, {"15:00": 1}, {"16:00": 1}, {"17:00": 1}, {"18:00": 1}],
-        'joice': [{"06:30": 1}, {"09:00": 1}, {"11:30": 1}, {"14:00": 1}, {"16:30": 1}, {"19:00": 1}],
-        'malaman': [{"08:00": 1}, {"09:00": 1}, {"10:00": 1}, {"11:00": 1}, {"12:00": 1}, {"13:00": 1}, {"14:00": 1}, {"15:00": 1}, {"16:00": 1}, {"17:00": 1}, {"18:00": 1}],
-        'malaman-sabado': [{"07:00": 1}, {"08:00": 1}, {"09:00": 1}, {"10:00": 1}, {"11:00": 1}, {"12:00": 1}, {"13:00": 1}, {"14:00": 1}]
+        'hareware': {"08:00": 1, "09:00": 1, "10:00": 1, "11:00": 1, "13:00": 1, "14:00": 1, "15:00": 1, "16:00": 1, "17:00": 1, "18:00": 1},
+        'joice': {"06:30": 1, "09:00": 1, "11:30": 1, "14:00": 1, "16:30": 1, "19:00": 1},
+        'malaman': {"08:00": 1, "09:00": 1, "10:00": 1, "11:00": 1, "12:00": 1, "13:00": 1, "14:00": 1, "15:00": 1, "16:00": 1, "17:00": 1, "18:00": 1},
+        'malaman-sabado': {"07:00": 1, "08:00": 1, "09:00": 1, "10:00": 1, "11:00": 1, "12:00": 1, "13:00": 1, "14:00": 1}
     }
 
     tz = pytz.timezone('America/Sao_Paulo')
@@ -16,21 +14,26 @@ def verificar_horarios(env, agendamentos, data_agendamento):
     horario_atual_str = horario_atual.strftime('%H:%M')
     data_atual = horario_atual.strftime('%d/%m/%Y')
 
-    horarios_disponiveis_finais = []
+    data_agendamento_dt = datetime.strptime(data_agendamento, "%d/%m/%Y").date()
+    data_atual_dt = datetime.strptime(data_atual, "%d/%m/%Y").date()
 
-    if data_agendamento == data_atual:
-        for horario_disponivel in horarios_disponiveis[env]:
-            hora = list(horario_disponivel.keys())[0]
-            if hora > horario_atual_str:
-                horarios_disponiveis_finais.append(horario_disponivel)
+    if env not in horarios_disponiveis:
+        return []
 
-    elif data_agendamento > data_atual:
-        horarios_disponiveis_finais = horarios_disponiveis[env]
+    horarios_disponiveis_finais = {}
+
+    if data_agendamento_dt == data_atual_dt:
+        for hora, disponivel in horarios_disponiveis[env].items():
+            if datetime.strptime(hora, "%H:%M").time() > horario_atual.time():
+                horarios_disponiveis_finais[hora] = disponivel
+    elif data_agendamento_dt > data_atual_dt:
+        horarios_disponiveis_finais = horarios_disponiveis[env].copy()
 
     for agendamento in agendamentos:
         horario_agendado = agendamento[:5]
-        horarios_disponiveis_finais = [horario for horario in horarios_disponiveis_finais if list(horario.keys())[0] != horario_agendado]
+        if horario_agendado in horarios_disponiveis_finais:
+            del horarios_disponiveis_finais[horario_agendado]
 
-    horarios_livres = [list(horario.keys())[0] for horario in horarios_disponiveis_finais if list(horario.values())[0] == 1]
+    horarios_livres = [hora for hora, disponivel in horarios_disponiveis_finais.items() if disponivel == 1]
 
     return horarios_livres
