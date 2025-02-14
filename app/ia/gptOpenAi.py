a816b8d5118d3ca405113d3cdb1b9112901094be
giordanoenzo <enzogiordanoofficial@gmail.com>
2025-02-14 14:35:23 -0300

    atualização

--- app/ia/gptOpenAi.py
+++ app/ia/gptOpenAi.py
@@ -2,16 +2,27 @@ from openai import OpenAI
 import requests
 
 
-client = OpenAI(api_key="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")
-
 assistant_id_openai = {
-    "hareware": "asst_pb8dfn9OtyXz4c0s9pZMOXSQ",
-    "emyconsultorio": "asst_XnR71BKSAU4dceGOranD7qJb"
-    }
+    "hareware": [
+        "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",
+        "asst_pb8dfn9OtyXz4c0s9pZMOXSQ"
+    ],
+    "emyconsultorio": [
+        "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",
+        "asst_I7AwY6tfrzdqIPQ2xsImgKk3"
+    ]
+}
 
 
 def ask_to_openai(code, pergunta):
     try:
+        if code not in assistant_id_openai:
+            return "Erro: Código do assistente inválido."
+
+        api_key, assistant_id = assistant_id_openai[code]
+
+        client = OpenAI(api_key=api_key)
+
         thread = client.beta.threads.create()
 
         client.beta.threads.messages.create(
@@ -22,23 +33,24 @@ def ask_to_openai(code, pergunta):
 
         run = client.beta.threads.runs.create_and_poll(
             thread_id=thread.id,
-            assistant_id=assistant_id_openai[code]
+            assistant_id=assistant_id
         )
 
-        if run.status == 'completed':
+        if run.status == "completed":
             messages = client.beta.threads.messages.list(thread_id=thread.id)
             return messages.data[0].content[0].text.value
-        elif run.status == 'failed':
-            return "Desculpa, mas meu sistema cognitivo falhou, poderia escrever novamente a sua mensagem?"
-        elif run.status == 'incomplete':
-            return "Desculpa, não consegui compreender o que você disse, poderia dizer novamente porém de outra forma?"
+        elif run.status == "failed":
+            return "Desculpa, mas meu sistema cognitivo falhou. Poderia escrever novamente sua mensagem?"
+        elif run.status == "incomplete":
+            return "Desculpa, não consegui compreender. Poderia reformular sua pergunta?"
         else:
             return f"Erro: {run.status}"
+
     except requests.exceptions.Timeout:
         return "Erro de conexão: O tempo de espera pela resposta da API excedeu o limite."
     except requests.exceptions.RequestException as e:
         return f"Erro de requisição: {str(e)}"
     except KeyError:
-        return "Erro: O código do assistente fornecido é inválido."
+        return "Erro: Código do assistente inválido."
     except Exception as e:
         return f"Erro inesperado: {str(e)}"
