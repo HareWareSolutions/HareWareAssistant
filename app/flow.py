bc989d9bf2832d2ee1689a50b18000d4cec94cf7
giordanoenzo <enzogiordanoofficial@gmail.com>
2025-02-21 18:39:57 -0300

    ajuste

--- app/flow.py
+++ app/flow.py
@@ -61,7 +61,7 @@ def fluxo_conversa(env, prompt, telefone, nome_contato: str = None, id_contrato:
                         return {"CAG": agendamentos}
 
                 if env == 'emyconsultorio':
-                    novo_status = gravar_status((db, telefone, "EPC", datetime.now().time()), None, None)
+                    novo_status = gravar_status(db, telefone, "EPC", datetime.now().time(), None, None)
 
                     return {"EPC", ["Profilaxia Dental (Limpeza)", "Clareamento a Laser (Em consultório)", "Restauração em Resina", "Extração de Dente", "Raspagem Gengival", "Atendimento Infantil", "Assessoria em Aleitamento Materno", "Outro"]}
                 else:
